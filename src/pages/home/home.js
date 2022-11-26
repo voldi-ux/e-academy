fc36629984d95fa9684cc0eb3450ddb50c682983
S-ciz <95995178+S-ciz@users.noreply.github.com>
2022-11-26 16:48:24 +0200

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,12 @@
 /** @format */
 
-import React from "react";
-
 // you guys can replace the h1 with the respective components you have built. no need to touch app.js file.
 // I will implemnting routing as soon as possible.
 
+import Questions from '../../components/QuestionComponent/Questions'
+
 const Home = () => {
-  return <h1>Home page</h1>;
+  return <Questions/>
 };
 
 export default Home;
