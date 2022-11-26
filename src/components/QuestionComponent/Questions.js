a56c7f75189b75512262ce0f19c204b55cabd27d
S-ciz <95995178+S-ciz@users.noreply.github.com>
2022-11-26 16:46:26 +0200

    Add files via upload

---
+++ src/components/QuestionComponent/Questions.js
@@ -0,0 +1,22 @@
+
+import './App.css'
+import Question from "./Question"
+
+
+const Questions = () => {
+  return (
+
+    <div className="questions">
+        <div id="questions">
+
+        <div><Question/></div>
+        <div><Question/></div>
+        <div><Question/></div>
+        
+        </div>
+
+    </div>
+  )
+}
+
+export default Questions
