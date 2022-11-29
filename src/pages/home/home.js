47ac064f1d00f4bc4c2666f2ba232d207839ad5b
S-ciz <95995178+S-ciz@users.noreply.github.com>
2022-11-29 10:26:05 +0200

    Add files via upload

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,15 +3,19 @@
 // you guys can replace the h1 with the respective components you have built. no need to touch app.js file.
 // I will implemnting routing as soon as possible.
 
+/*
 import Blocks from '../../components/listBlocks/blocks';
 import Questions from '../../components/QuestionComponent/Questions'
 import Slider from '../../components/slider/slider';
 
+*/
+
+import Notifications from '../../components/Notification/Notifications/Notifications'
+
 const Home = () => {
   return (
     <div>
-      <Slider />
-      <Blocks />
+      <Notifications/>
     </div>
   );
 };
