0ab5565838fd67bc7400a9d6aa004d7a7bb4d09c
voldi <itsvee156@gmail.com>
2022-12-06 17:52:50 +0200

    testing editor component

---
+++ src/components/EitorMain/mainEditor.js
@@ -0,0 +1,56 @@
+/** @format */
+
+import React from "react";
+import "./mainEditor.css";
+import { IconContext } from "react-icons";
+import { BsFillImageFill, BsTypeBold, BsTypeUnderline, BsTextLeft, BsTextCenter } from "react-icons/bs";
+import { AiFillSave } from "react-icons/ai";
+
+const MainEditor = () => {
+  return (
+    <div className="main-editor-container">
+      <header className="main-editor-header">
+        <IconContext.Provider value={{ className: "editor-icons-left", size: 20 }}>
+          <div className="main-editor-icons-left">
+            <div className="icons-container">
+              <BsFillImageFill />
+              <small>insert</small>
+            </div>
+            <div className="icons-container">
+              <BsTypeBold />
+              <small>Bold</small>
+            </div>
+            <div className="icons-container">
+              <BsTypeUnderline />
+              <small>underlined</small>
+            </div>
+            <div className="icons-container">
+              <div className="icon-align-text">
+                <BsTextLeft />
+                <BsTextCenter />
+              </div>
+              <small>align</small>
+            </div>
+          </div>
+          <div className="editor-icon-righ">
+            <div className="icons-container">
+              <AiFillSave />
+              <small>save</small>
+            </div>
+          </div>
+        </IconContext.Provider>
+          </header>
+          <div className="main-editor-contents" >
+              <div className="main-editor-content">
+                  
+              </div>
+              <div className="main-editor-preview">
+              
+              </div>
+             
+          </div>
+    </div>
+  );
+};
+
+export default MainEditor;
