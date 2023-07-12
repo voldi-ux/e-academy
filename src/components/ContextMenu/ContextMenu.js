560a254aec6e7ab4369b8ebfb383621b7d658209
S-ciz <mushinganyacizungu@gmail.com>
2023-07-12 15:26:53 +0200

    context-menu

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -4,28 +4,30 @@ import "./contextMenu.css";
 import ContextMenuItem from "./ContextMenuItem";
 import { IconContext } from "react-icons";
 
-
-const ContextMenu = ({ contextMenu }) => { 
+const ContextMenu = ({ contextMenu }) => {
   const renderMenus = () =>
     contextMenu.map((contextMenuItem) => (
-      <ContextMenuItem key={contextMenuItem.title} menus={contextMenuItem.options} menuTitle={contextMenuItem.title}>
+      <ContextMenuItem
+        key={contextMenuItem.title}
+        menus={contextMenuItem.options}
+        menuTitle={contextMenuItem.title}
+      >
         {" "}
         {contextMenuItem.icon}
       </ContextMenuItem>
     ));
-    return (
-      <div className="context-menu-container">
-        <IconContext.Provider value={{ size: 15 }}>
-           {/* <ContextMenuItem MenuTitle={"Capitalize"}> <AiOutlineFontSize  /> </ContextMenuItem>
+  return (
+    <div className="context-menu-container">
+      <IconContext.Provider value={{ size: 15 }}>
+        {/* <ContextMenuItem MenuTitle={"Capitalize"}> <AiOutlineFontSize  /> </ContextMenuItem>
            <ContextMenuItem MenuTitle={"Font Size"}> <VscTextSize /> </ContextMenuItem>
            <ContextMenuItem  MenuTitle={"Bold"}> <AiOutlineBold /> </ContextMenuItem>
            <ContextMenuItem MenuTitle={"Color"}>  <AiOutlineBgColors /> </ContextMenuItem> */}
-          
-          { renderMenus()}
-        </IconContext.Provider>
-      </div>
-    );
-}
 
+        {renderMenus()}
+      </IconContext.Provider>
+    </div>
+  );
+};
 
 export default ContextMenu;
--- src/components/ContextMenu/ContextMenuItem.js
+++ src/components/ContextMenu/ContextMenuItem.js
@@ -1,35 +1,53 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./contextMenu.css";
 import MenuItem from "./MenuItem";
 
 const ContextMenuItem = ({ menus, menuTitle, children }) => {
-  const renderMenuItems = () => { 
-     if(menuTitle.toLowerCase() === "change color") return  menus.map((menu) => (
-       <MenuItem menu={menu} key={menu.option} color={true} />
-     ))
-    
+  const renderMenuItems = () => {
+    if (menuTitle.toLowerCase() === "change color")
+      return menus.map((menu) => (
+        <MenuItem menu={menu} key={menu.option} color={true} />
+      ));
+
     return menus.map((menu) => <MenuItem key={menu.option} menu={menu} />);
-  }
+  };
+  const menuContainer = useRef();
+  const menuItem = useRef();
+
+  const showMenu = () => {
+    if (menuContainer.current.style.display == "block") {
+      menuContainer.current.style.display = "none";
+      menuItem.current.style.maxHeight = "0";
+    } else {
+      menuContainer.current.style.display = "block";
+
+      menuItem.current.style.maxHeight = "6rem";
+    }
+  };
   return (
-    <div className="context-menu-item">
+    <div onClick={showMenu} className="context-menu-item">
       <div className="context-menu-item-content">
         <div className="context-menu-item-icon">{children}</div>
         <h3> {menuTitle} </h3>
       </div>
-        <div className="menu-container">
-         <div className="menu-container-inner">
-          { renderMenuItems()}
-         </div>
+      <div ref={menuItem} className="menu-container">
+        <div
+          ref={menuContainer}
+          onClick={showMenu}
+          className="menu-container-inner"
+        >
+          {renderMenuItems()}
         </div>
+      </div>
     </div>
   );
 };
 
 ContextMenuItem.defaultProps = {
   menus: ["Upper Case", "Lower Case", "Capitalize"],
-  menuTitle: "Title"
+  menuTitle: "Title",
 };
 
 export default ContextMenuItem;
